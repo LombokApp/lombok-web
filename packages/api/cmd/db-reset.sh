161db0892c6fe6ed77683c9f43dd151a5383f4ab
Steven Kampen <steven@peertjelabs.nl>
2023-10-28 00:00:51 +0200

    Fix db reset and migrate up scripts

--- packages/api/cmd/db-reset.sh
+++ packages/api/cmd/db-reset.sh
@@ -4,6 +4,6 @@ set -e
 set -x
 
 yarn ts-node "./script/db-reset.ts"
-yarn drizzle-kit up:pg --config ./src/orm/drizzle.config.ts
+yarn ts-node "./script/db-migrate-up.ts"
 
 { set +x; } 2>/dev/null
