db4a8403aaa116efc5d38b47b5084b5757fe311d
Steven Kampen <steven@peertjelabs.nl>
2024-02-27 20:29:31 +0100

    More working

--- packages/nestjsapi/src/generate-metadata.ts
+++ packages/nestjsapi/src/generate-metadata.ts
@@ -1,12 +1,30 @@
 import { PluginMetadataGenerator } from '@nestjs/cli/lib/compiler/plugins'
 import { ReadonlyVisitor } from '@nestjs/swagger/dist/plugin'
+import fs from 'fs/promises'
+import path from 'path'
 
 const generator = new PluginMetadataGenerator()
-generator.generate({
-  visitors: [
-    new ReadonlyVisitor({ introspectComments: true, pathToSource: __dirname }),
-  ],
-  outputDir: __dirname,
-  printDiagnostics: true,
-  tsconfigPath: 'tsconfig.json',
-})
+
+async function main() {
+  const generatedDir = path.join(__dirname, 'generated')
+  const generatedDirStat = await fs.stat(generatedDir)
+
+  if (!generatedDirStat.isDirectory()) {
+    await fs.mkdir(generatedDir)
+  }
+
+  generator.generate({
+    visitors: [
+      new ReadonlyVisitor({
+        introspectComments: true,
+        pathToSource: __dirname,
+      }),
+    ],
+    outputDir: __dirname,
+    printDiagnostics: true,
+    tsconfigPath: 'tsconfig.json',
+    filename: 'generated/metadata.ts',
+  })
+}
+
+void main()
