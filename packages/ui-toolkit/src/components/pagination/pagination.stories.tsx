41180ac98a6ecd796cd0d4629070ec5d0664fdcd
Steven Kampen <steven@peertjelabs.nl>
2024-10-11 23:15:43 +0200

    Add remaining shadcn components

---
+++ packages/ui-toolkit/src/components/pagination/pagination.stories.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+import type { Meta, StoryObj } from '@storybook/react'
+
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from '..'
+
+const meta: Meta<typeof Pagination> = {
+  title: 'Components/Pagination',
+  component: Pagination,
+}
+
+export default meta
+
+type Story = StoryObj<typeof Pagination>
+
+export const BasicUsage: Story = {
+  args: {},
+  render: () => (
+    <Pagination>
+      <PaginationContent>
+        <PaginationItem>
+          <PaginationPrevious onClick={() => console.log('previous clicked')} />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink
+            onClick={() => console.log('pagination jump link clicked')}
+          >
+            1
+          </PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationEllipsis />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationNext onClick={() => console.log('next clicked')} />
+        </PaginationItem>
+      </PaginationContent>
+    </Pagination>
+  ),
+}
