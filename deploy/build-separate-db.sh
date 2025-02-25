65588475256d4d8b7a5f143f212f9981f7a60b69
Steven Kampen <steven@peertjelabs.nl>
2025-02-25 15:42:38 +0100

    STE-10 feat: Standalone docker build + npm script + compose file

---
+++ deploy/build-separate-db.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+BRANCH=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD | sed 's/[^A-Za-z0-9_\.-]/--/g' | head -c100)
+SHA1=$(git rev-parse --short HEAD)
+VERSION=$BRANCH-$SHA1-$(date +%s)
+NAME=stellariscloud-standalone
+
+echo "Building version: $VERSION"
+docker build --platform linux/amd64 --target release -t $NAME:$VERSION -f "../docker/app.Dockerfile" ../
