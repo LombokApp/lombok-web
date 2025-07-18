f71955c4cb90d37c459477133d2f9052b816374a
Steven Kampen <steven@peertjelabs.nl>
2025-07-18 06:50:06 +0200

    Move generated openapi "paths", and extracted api type references, into types package

--- packages/stellaris-types/src/types/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './apps.types'
-export * from './content.types'
-export * from './folder.types'
-export * from './messages.types'
-export * from './storage.types'
-export * from './events.types'
