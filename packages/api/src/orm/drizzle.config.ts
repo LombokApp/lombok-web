e0a8db5ba7ae46a055021bbb32a9fba9c219f68d
Steven Kampen <steven@peertjelabs.nl>
2025-07-26 21:41:29 +0200

    STE-70 (chore): indexes on `size` & `mediaType` & `mediaType` (#57)

--- packages/api/src/orm/drizzle.config.ts
+++ packages/api/src/orm/drizzle.config.ts
@@ -2,8 +2,8 @@ import type { Config } from 'drizzle-kit'
 import path from 'path'
 
 export default {
-  schema: path.join(import.meta.dirname, `../**/entities/*.entity.ts`),
-  out: path.join(import.meta.dirname, `migrations`),
+  schema: path.join(__dirname, `../**/entities/*.entity.ts`),
+  out: path.join(__dirname, `migrations`),
   dialect: 'postgresql',
   dbCredentials: {
     url: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
