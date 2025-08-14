15a8dd141337ea1b6f05f5012134c61e15e02d65
Steven Kampen <steven@peertjelabs.nl>
2025-08-14 14:43:12 +0200

    STE-109 (chore): add GH actions workflows for publishing to ghcr.io (#81)

--- packages/api/src/orm/config/orm.config.ts
+++ packages/api/src/orm/config/orm.config.ts
@@ -2,21 +2,31 @@ import { registerAs } from '@nestjs/config'
 import { isBoolean, isInteger, parseEnv } from 'src/platform/utils/config.util'
 import { z } from 'zod'
 
+const EMBEDDED_POSTGRES = process.env.EMBEDDED_POSTGRES === 'true'
+
 export const ormConfig = registerAs('orm', () => {
   const env = parseEnv({
-    DB_HOST: z.string(),
-    DB_USER: z.string(),
-    DB_PORT: z.string().refine(isInteger),
-    DB_NAME: z.string(),
-    DB_PASSWORD: z.string(),
+    DB_HOST: EMBEDDED_POSTGRES
+      ? z.undefined({
+          message: 'Should not set DB_HOST when EMBEDDED_POSTGRES=true',
+        })
+      : z.string().nonempty(),
+    DB_USER: z.string().nonempty(),
+    DB_PORT: EMBEDDED_POSTGRES
+      ? z.undefined({
+          message: 'Should not set DB_PORT when EMBEDDED_POSTGRES=true',
+        })
+      : z.string().refine(isInteger),
+    DB_NAME: z.string().nonempty(),
+    DB_PASSWORD: z.string().nonempty(),
     DISABLE_NOTICE_LOGGING: z.string().refine(isBoolean).optional(),
     RUN_MIGRATIONS: z.string().refine(isBoolean).optional(),
     LOG_QUERIES: z.string().refine(isBoolean).optional(),
     CREATE_DATABASE: z.string().refine(isBoolean).optional(),
   })
   return {
-    dbHost: env.DB_HOST,
-    dbPort: parseInt(env.DB_PORT, 10),
+    dbHost: env.DB_HOST ?? 'localhost',
+    dbPort: env.DB_PORT ? parseInt(env.DB_PORT, 10) : 5432,
     dbUser: env.DB_USER,
     dbPassword: env.DB_PASSWORD,
     dbName: env.DB_NAME,
